2d5842d9ae8dfcfa8a9c3fbafc1f447e59961d08
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-12-25 03:30:40 +0500

    fixes

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -11,7 +11,7 @@ export async function POST(req) {
           message: "Name, email, and message are required fields.",
           success: false,
         },
-        { status: 400 }
+        { status: 400 } 
       )
     }
     const transporter = nodemailer.createTransport({
